f4a79cb2fccae44c389a8b871bb4a2791df3c108
Rohitprajapati22 <rohitt3891@gmail.com>
2024-01-30 12:08:28 +0530

    lec-9

---
+++ program-9.2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+
+	int n;
+	
+	for(n=10;n >= 1; n--){
+		printf("%d ",n);
+	}
+	
+}
---
+++ program-9.3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+
+	int i=1,n;
+	
+	printf("Enter any number :- ");
+	scanf("%d",&n);
+	
+	do{
+		
+		if(i%2==0){
+			printf("%d ",i);
+		}
+		i++;
+		
+	}while(i <= n);
+	
+}
